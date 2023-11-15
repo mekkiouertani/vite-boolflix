c3b585314d01ce5830829946b382ad79474cb577
Mekki Ouertani <140953094+mekkiouertani@users.noreply.github.com>
2023-11-15 18:33:31 +0100

    add v-for and MainComponent

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,17 @@
 <template>
   <TitleComponent />
+  <MainComponent />
 </template>
 
 <script>
 import { store } from "./data/store.js";
 import axios from "axios";
 import TitleComponent from './components/TitleComponent.vue';
+import MainComponent from './components/MainComponent.vue';
 
 export default {
   name: 'App',
-  components: { TitleComponent },
+  components: { TitleComponent, MainComponent },
   data() {
     return {
       store,
@@ -20,15 +22,23 @@ export default {
       const url = this.store.apiUrl + this.store.endPoint.movie;
       axios.get(url, { params: this.store.params }).then((res) => {
         console.log(res.data.results);
+        this.store.movieList = res.data.results
       })
-
+    },
+    getTv() {
+      const url = this.store.apiUrl + this.store.endPoint.series;
+      axios.get(url, { params: this.store.params }).then((res) => {
+        console.log(res.data.results);
+        this.store.seriesList = res.data.results
+      })
     }
   },
   mounted() {
-
+    this.getMovies();
+    this.getTv();
   },
   created() {
-    this.getMovies();
+
   },
 
 }
---
+++ src/components/MainComponent.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="container">
+        <section id="movie" class="mt-5">
+            <h2>Movie</h2>
+            <div class="row">
+                <div v-for="movie in store.movieList" class="col-12 col-md-4 col-lg-3">
+                    {{ movie.title }}
+                </div>
+            </div>
+        </section>
+        <section id="series" class="mt-5">
+            <h2>Series</h2>
+            <div class="row">
+                <div v-for="series in store.seriesList" class="col-12 col-md-4 col-lg-3">
+                    {{ series.name }}
+                </div>
+            </div>
+        </section>
+
+    </div>
+</template>
+
+<script>
+import { store } from "../data/store.js";
+export default {
+    name: 'MainComponent',
+    data() {
+        return {
+            store,
+        }
+    },
+}
+</script>
+
+<style lang="scss" scoped></style>
