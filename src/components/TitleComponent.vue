9b692e128e2173c84ea06189af55b0d2fb53f03a
Mekki Ouertani <140953094+mekkiouertani@users.noreply.github.com>
2023-11-15 16:22:21 +0100

    updated TitleComponent

--- src/components/TitleComponent.vue
+++ src/components/TitleComponent.vue
@@ -1,6 +1,9 @@
 <template>
-    <div>
-        <h1>titolo</h1>
+    <div class="container-fluid bg-black p-3 d-flex justify-content-between ">
+        <h1>Boolflix</h1>
+        <div>
+            <input type="text" class="fs-4 mt-3" placeholder="Cerca..">
+        </div>
     </div>
 </template>
 
@@ -10,4 +13,19 @@ export default {
 }
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+h1 {
+    color: red;
+    font-size: 3em;
+}
+
+input[type=text] {
+    padding: 13px 20px;
+    margin: 8px 0;
+    box-sizing: border-box;
+    border: none;
+    background-color: transparent;
+    color: white;
+    height: 20px;
+}
+</style>
